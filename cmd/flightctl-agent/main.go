4598522b9b0dcf2b121cedd5c865a2aaf089d7c7
Lily Sturmann <lkatalin@users.noreply.github.com>
2026-03-07 20:32:22 -0500

    Revert "Skip sending IMA measurement list for allowlist-based policies"
    This reverts commit e71fb7c78072058fd0531474f443c6d33115bc57.

--- cmd/flightctl-agent/main.go
+++ cmd/flightctl-agent/main.go
@@ -106,7 +106,7 @@ func NewAgentCommand() *agentCmd {
 func (a *agentCmd) Execute() error {
 	// DEBUG VERSION TRACKER - Update this with each code change to verify running version
 	a.log.Infof("========================================")
-	a.log.Infof("AGENT DEBUG VERSION: v9 - Allowlist IMA policy (skip measurement list)")
+	a.log.Infof("AGENT DEBUG VERSION: v8 - Fixed TPML_DIGEST to always write 8 slots")
 	a.log.Infof("========================================")
 
 	agentInstance := agent.New(a.log, a.config, a.configFile)
